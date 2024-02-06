05c1538bbd2867c6f6cf7637441253f42a253b5d
AngelofPadoan <134550505+AngelofPadoan@users.noreply.github.com>
2024-02-06 12:00:53 -0300

    initial commit

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema(
+  {
+    nombre: { type: String, trim: true },
+    apellido: { type: String, trim: true },
+    edad: { type: Number, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+UserSchema.methods.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+UserSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default mongoose.model("User", UserSchema);
